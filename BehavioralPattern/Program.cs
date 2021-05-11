984741596a167c3ef06f4024723ebeff887fdfea
WisdomerLee <cheonnyeo@gmail.com>
2021-05-11 09:20:10 +0900

    행위 패턴 커맨드 추가
    interpreter는 내일 추가 예정

--- BehavioralPattern/Program.cs
+++ BehavioralPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 //행위 패턴 모음
 //객체, 클래스 사이의 알고리즘, 책임 분배에 관련된 패턴
 //한 객체가 혼자 수행할 수 없는 작업을 여러 개의 객체로 어떻게 분배하는지, 또 그렇게 하면서 객체 사이의 결합도를 최소화하는 것에 중점
@@ -216,12 +217,230 @@ namespace BehavioralPattern
     //실행될 기능을 캡슐화 하여 주어진 여러 기능을 실행할 수 있게 재사용성이 높은 클래스를 설계하는 패턴
     namespace Command
     {
+        #region 구조
+        /// <summary>
+        /// Command (Command) : 행동에 관한 인터페이스 정의
+        /// ConcreteCommand (CalculatorCommand) : 행동과 특정 파라미터를 받는 객체를 연결, 특정 행동을 불러오는 함수(받는 객체에 연결되는)를 정의
+        /// Client (CommandApp) : ConcreteCommand객체를 생성하고 받는 객체로 설정
+        /// Invoker (User) : 행동이 실행되도록 요청
+        /// Receiver (Calculator) : 요청에 따라 동작이 어떻게 진행되는지 정의되어있음
+        /// </summary>
+        class CommandStructuralMain
+        {
+            static void Main()
+            {
+                Receiver receiver = new Receiver();
+                StructureCommand command = new ConcreteCommand(receiver);
+                Invoker invoker = new Invoker();
 
+                invoker.SetCommand(command);
+                invoker.ExecuteCommand();
+
+                Console.ReadKey();
+            }
+        }
+
+        abstract class StructureCommand
+        {
+            protected Receiver receiver;
+
+            public StructureCommand(Receiver receiver)
+            {
+                this.receiver = receiver;
+            }
+            public abstract void Execute();
+        }
+
+        class ConcreteCommand : StructureCommand
+        {
+            public ConcreteCommand(Receiver receiver) : base(receiver)
+            {
+
+            }
+            public override void Execute()
+            {
+                receiver.Action();
+            }
+        }
+
+        class Receiver
+        {
+            public void Action()
+            {
+                Console.WriteLine("Called Receiver.Action()");
+            }
+        }
+
+        class Invoker
+        {
+            StructureCommand command;
+
+            public void SetCommand(StructureCommand command)
+            {
+                this.command = command;
+            }
+
+            public void ExecuteCommand()
+            {
+                command.Execute();
+            }
+        }
+        #endregion
+        #region 실제 사례
+        class CommandRealMain
+        {
+            static void Main()
+            {
+                User user = new User();
+
+                user.Compute('+', 100);
+                user.Compute('-', 50);
+                user.Compute('*', 10);
+                user.Compute('/', 2);
+
+                user.Undo(4);
+
+                user.Redo(3);
+
+                Console.ReadKey();
+            }
+        }
+
+        abstract class CommandReal
+        {
+            public abstract void Execute();
+            public abstract void UnExecute();
+        }
+
+        class CalculatorCommand : CommandReal
+        {
+            char _operator;
+            int operand;
+            Calculator calculator;
+
+            public CalculatorCommand(Calculator calculator, char @operator, int operand)
+            {
+                this.calculator = calculator;
+                this._operator = @operator;
+                this.operand = operand;
+            }
+            public char Operator
+            {
+                set { _operator = value; }
+            }
+            public int Operand
+            {
+                set { operand = value; }
+            }
+
+            public override void Execute()
+            {
+                calculator.Operation(_operator, operand);
+            }
+            public override void UnExecute()
+            {
+                calculator.Operation(Undo(_operator), operand);
+            }
+
+            char Undo(char @operator) =>
+            
+                @operator switch
+                {
+                    '+' => '-',
+                    '-' => '+',
+                    '*' => '/',
+                    '/' => '*',
+                    _ => throw new ArgumentException("@operator"),
+                };
+        }
+
+        class Calculator
+        {
+            int curr = 0;
+
+            public void Operation(char @operator, int operand)
+            {
+                switch (@operator)
+                {
+                    case '+':
+                        curr += operand;
+                        break;
+                    case '-':
+                        curr -= operand;
+                        break;
+                    case '*':
+                        curr *= operand;
+                        break;
+                    case '/':
+                        curr /= operand;
+                        break;
+                }
+                Console.WriteLine($"Current value = {curr,3} (following {@operator} {operand})");
+            }
+        }
+
+        class User
+        {
+            Calculator calculator = new Calculator();
+            List<CommandReal> commands = new List<CommandReal>();
+            int current = 0;
+
+            public void Redo(int levels)
+            {
+                Console.WriteLine($"\n----Redo {levels} level");
+                for(int i = 0; i<levels; i++)
+                {
+                    if (current < commands.Count - 1)
+                    {
+                        CommandReal command = commands[current++];
+                        command.Execute();
+                    }
+                }
+            }
+
+            public void Undo(int levels)
+            {
+                Console.WriteLine($"\n----Undo {levels} level");
+                for(int i = 0; i<levels; i++)
+                {
+                    if (current > 0)
+                    {
+                        CommandReal command = commands[--current] as CommandReal;
+                        command.UnExecute();
+                    }
+                }
+            }
+
+            public void Compute(char @operator, int operand)
+            {
+                CommandReal command = new CalculatorCommand(calculator, @operator, operand);
+                command.Execute();
+
+                commands.Add(command);
+                current++;
+            }
+        }
+
+
+        #endregion
     }
-
+    //단어를 주고 해당 표현의 문법을 정의하고 표현된 문법의 해석을 달아두는 것...
     namespace Interpreter
     {
+        #region 구조
+        /// <summary>
+        /// AbstractExpression (Expression) : 실행되어야 할 함수 인터페이스 정의
+        /// TerminalExpression (ThousandExpression, HundredExpression, TenExpression, OneExpression) : 전달되는 끝의 객체 문법에서 해석되는 행동들이 정의되어있음, 모든 객체는 문장에 모든 끝맺음 기호가 있어야 함
+        /// NonterminalExpression (not used) : 
+        /// </summary>
+        class InterpreterStructuralMain
+        {
+            
+        }
+        #endregion
+        #region 실제 사례
 
+        #endregion
     }
 
     namespace Iterator
